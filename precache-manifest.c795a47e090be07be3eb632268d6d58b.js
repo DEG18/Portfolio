071fac4b19483d28134fc29513bee7777a3b8115
Luyu Zeng <zengluyu252@gmail.com>
2020-09-02 15:39:02 -0400

    Updates

---
+++ precache-manifest.c795a47e090be07be3eb632268d6d58b.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06af71d00a3ddaabf25bff640aa56e68",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f8451a8284b0de485e64",
+    "url": "/Portfolio/static/css/main.adb8bea8.chunk.css"
+  },
+  {
+    "revision": "969be490156decdf3f2f",
+    "url": "/Portfolio/static/js/2.de472787.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.de472787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8451a8284b0de485e64",
+    "url": "/Portfolio/static/js/main.aea0a6ca.chunk.js"
+  },
+  {
+    "revision": "fd8d686779d15b6056f8",
+    "url": "/Portfolio/static/js/runtime-main.6afc4808.js"
+  },
+  {
+    "revision": "578cc48e697cd4942119f37ecedb8847",
+    "url": "/Portfolio/static/media/Circle2.578cc48e.jpg"
+  },
+  {
+    "revision": "6d95f0b8a57004c5460aba43d46a66aa",
+    "url": "/Portfolio/static/media/CoverProjects.6d95f0b8.jpg"
+  },
+  {
+    "revision": "2318de73957dd3158232cc809126d0f9",
+    "url": "/Portfolio/static/media/CoverSport.2318de73.jpg"
+  },
+  {
+    "revision": "54c9d55162fb314e48d98dfe0d98f203",
+    "url": "/Portfolio/static/media/CoverTem2.54c9d551.jpg"
+  },
+  {
+    "revision": "21d9d57f997e58c05e6c913e234fdc46",
+    "url": "/Portfolio/static/media/CoverTravel.21d9d57f.jpg"
+  },
+  {
+    "revision": "b7d05b701fc9b9b0ecc0d3bf1a69e26e",
+    "url": "/Portfolio/static/media/Email3.b7d05b70.jpg"
+  },
+  {
+    "revision": "28e580afdb6772b1d604985149943049",
+    "url": "/Portfolio/static/media/HomepageAbout2.28e580af.jpg"
+  },
+  {
+    "revision": "f6d77d486e59fd7019d78a0d54f22bf9",
+    "url": "/Portfolio/static/media/HomepageAbout3.f6d77d48.jpg"
+  },
+  {
+    "revision": "63415f1f1e194efc8fdbb4b644db7dee",
+    "url": "/Portfolio/static/media/HomepageContact2.63415f1f.jpg"
+  },
+  {
+    "revision": "bced88d51609c783396ebb30acd4079d",
+    "url": "/Portfolio/static/media/HomepageTravel2.bced88d5.jpg"
+  },
+  {
+    "revision": "b7f328ac205d0f3c8329ec991f589d47",
+    "url": "/Portfolio/static/media/HomepageTravel3.b7f328ac.jpg"
+  },
+  {
+    "revision": "dd9a00142488b95b9d7765348bd6e3ec",
+    "url": "/Portfolio/static/media/Kart.dd9a0014.jpg"
+  },
+  {
+    "revision": "60dbbb20d8f03351ceab4e074eadc5d1",
+    "url": "/Portfolio/static/media/Labyrinth-hike1.60dbbb20.jpg"
+  },
+  {
+    "revision": "6af656175f34e0835b8a064d43c630e9",
+    "url": "/Portfolio/static/media/Logo3.6af65617.png"
+  },
+  {
+    "revision": "febdf8f74ef134c252d65a948b185258",
+    "url": "/Portfolio/static/media/Navbar.febdf8f7.jpg"
+  },
+  {
+    "revision": "d3aa20dc37b655d34b3e00553a0c08b1",
+    "url": "/Portfolio/static/media/PawsCity_logo2.d3aa20dc.png"
+  },
+  {
+    "revision": "79d55f5bdb0e8ec300d088fdd58fb6cb",
+    "url": "/Portfolio/static/media/Pink2.79d55f5b.jpg"
+  },
+  {
+    "revision": "77eea04e1fc97411b842a1ed16284c8d",
+    "url": "/Portfolio/static/media/Travel2.77eea04e.jpg"
+  },
+  {
+    "revision": "e759ea9bd77b20b6d00962026016e729",
+    "url": "/Portfolio/static/media/Vegan_logo1.e759ea9b.png"
+  },
+  {
+    "revision": "35746d02408c0ed90f98a95bbbfbf641",
+    "url": "/Portfolio/static/media/badminton1.35746d02.jpg"
+  },
+  {
+    "revision": "29fedf3f26f9bcaaf5185aea49ff8c8a",
+    "url": "/Portfolio/static/media/badminton2.29fedf3f.jpg"
+  },
+  {
+    "revision": "5a68004425e7a6c15b496c907c156145",
+    "url": "/Portfolio/static/media/basketball.5a680044.jpg"
+  },
+  {
+    "revision": "43ea06a40fd89ccead15d7b826730518",
+    "url": "/Portfolio/static/media/basketball2.43ea06a4.jpg"
+  },
+  {
+    "revision": "ad889ed0f2cca04a013f8b563d91a11d",
+    "url": "/Portfolio/static/media/hiking.ad889ed0.jpg"
+  },
+  {
+    "revision": "d3b465d8a304b23d1b56f02cf7ef0a6d",
+    "url": "/Portfolio/static/media/sport.d3b465d8.jpg"
+  },
+  {
+    "revision": "401404f48b60be613e60d4717d42afdf",
+    "url": "/Portfolio/static/media/travel1.401404f4.png"
+  },
+  {
+    "revision": "33ef4169771471c13d7b5acfb8dc131a",
+    "url": "/Portfolio/static/media/travel2.33ef4169.png"
+  },
+  {
+    "revision": "96848dd789aec52a781376f5722b37ae",
+    "url": "/Portfolio/static/media/travel3.96848dd7.jpg"
+  }
+]);
